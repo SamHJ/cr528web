4186e427308f31f5a07806c1b083a57a75249d6b
Emenike Samuel <sam4gentle@gmail.com>
2021-12-04 14:40:03 +0100

    working on extra pages

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
+app.get("/why-georgia", function (request, response) {
+  response.sendFile(__dirname + '/public/why-georgia.html');
+});
+
+app.get("/tokenomics", function (request, response) {
+  response.sendFile(__dirname + '/public/tokenomics.html');
+});
+
 app.listen(port, () => {
   console.log(`app listening on http://localhost:${port}`)
 })
