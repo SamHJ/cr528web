8860ae09bdd2c7620d697b640377dc79d49604d9
Emenike Samuel <sam4gentle@gmail.com>
2021-12-04 13:33:43 +0100

    implementing requested changes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const path = require('path')
 const port = process.env.PORT || 3000
 
+
+app.use(cors())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => {
